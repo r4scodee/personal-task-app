8536377bd752d9c095ebd1f89d375e746650283c
rascodee <irbadhganzz@gmail.com>
2026-02-09 21:02:13 +0700

    app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,7 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->trustProxies(at: '*');
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
